af390bdb3d218de9c09e9c28beed59e15c53b60b
James <jdula1990@gmail.com>
2018-12-01 11:18:09 -0600

    added className instead of class

--- bookface/src/Components/Navbar/ComposeTextPost.component.js
+++ bookface/src/Components/Navbar/ComposeTextPost.component.js
@@ -5,24 +5,24 @@ import '../../Include/bootstrap';
 export class ComposeTextPostComponent extends React.Component{
    render(){
       return(
-          <div class="modal" tabindex="-1" role="dialog" id="post-text-modal">
-              <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title">Compose What is in your mind...</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <div className="modal" tabIndex="-1" role="dialog" id="post-text-modal">
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title">Compose What is in your mind...</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                       <span aria-hidden="true">&times;</span>
                     </button>
                   </div>
-                  <div class="modal-body">
+                  <div className="modal-body">
                     <ul className= "list-group list-group-flush" >
                         <li className="list-group-item flex-row-sb">
                           <textarea className="form-control" type="text" rows="1" id="posting-text-modal"></textarea>
                         </li> 
                     </ul>
                   </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-dismiss="modal">Post</button>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-danger" data-dismiss="modal">Post</button>
                   </div>
                 </div>
               </div>
